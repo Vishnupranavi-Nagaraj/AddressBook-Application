060934f4493ab043a868b583cf918066d7411086
Vishnupranavi-Nagaraj <110388091+Vishnupranavi-Nagaraj@users.noreply.github.com>
2022-08-04 00:45:27 +0530

    Delete address directory

--- address/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("$host", "localhost");
-define("$user", "root");
-define("$password", "Weakpass$12");
-define("$dbname", "addressbook");
-?>
--- address/view/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require '../model/Database.php';
-
-
-
-$sql=new Database();
-?>
-
